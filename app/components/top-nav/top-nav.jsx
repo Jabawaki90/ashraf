43566311ded71d0c23322c6e791e1f63d5e2060a
Ashraf Azemi <ashraf.chzm@gmail.com>
2023-06-07 23:02:32 +0800

    create profile dropdown container

--- app/components/top-nav/top-nav.jsx
+++ app/components/top-nav/top-nav.jsx
@@ -1,8 +1,23 @@
+"use client";
+import { useState } from "react";
 import "./top-nav.css";
 import SearchBox from "../search-box/search-box";
 import DarkmodeButton from "../darkmode-button/darkmode-button";
 
+const login = [
+  "Messages",
+  "History",
+  "Wishlists",
+  "Account",
+  "Help",
+  "Log out",
+];
+
+const notLogin = ["Sign up", "Log in", "Help"];
+
 const TopNav = () => {
+  const [dropdown, setDropdown] = useState(false);
+  const [login, setLogin] = useState(false);
   return (
     <div className="container-nav">
       <section className="left-container">
@@ -21,11 +36,28 @@ const TopNav = () => {
         <div style={{ marginLeft: "40px" }}>
           <i className="bi bi-bag" style={{ fontSize: 30, color: "white" }}></i>
         </div>
-        <div style={{ marginLeft: "40px" }}>
+        <div
+          onClick={() => {
+            if (dropdown) {
+              setDropdown(false);
+            } else {
+              setDropdown(true);
+            }
+          }}
+          style={{ marginLeft: "40px" }}
+        >
           <i
             className="bi bi-person-circle"
             style={{ fontSize: 40, color: "white" }}
           ></i>
+          {dropdown ? (
+            <section className="dropdown">
+              <div className="dropdown-child-container">
+                <h3>juh</h3>
+                <h3>juh</h3>
+              </div>
+            </section>
+          ) : null}
         </div>
       </section>
     </div>
